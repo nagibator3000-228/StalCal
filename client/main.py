e4ec28569b77307e8a389a086d810a40ba48209c
[PUZ]Cklebepand <116970894+nagibator3000-228@users.noreply.github.com>
2025-09-23 21:42:50 +0200

    added kill tab, fixed bugs, added gold deagle

--- client/main.py
+++ client/main.py
@@ -2,6 +2,7 @@ import socketio
 import json
 from ursina import *
 from ursina import Audio
+from direct.actor.Actor import Actor
 from ursina.shaders import lit_with_shadows_shader
 from ursina.shaders import basic_lighting_shader
 from ursina.prefabs.first_person_controller import FirstPersonController
@@ -16,6 +17,7 @@ notify-level error
 
 sio = socketio.Client()
 sio.connect('https://stalker-server-z2l9.onrender.com/', transports=['websocket'])
+# sio.connect('http://localhost:5000/', transports=['websocket'])
 
 other_players = {}
 tutorial = False
@@ -73,6 +75,12 @@ def player_left(sid):
         destroy(other_players[sid])
         del other_players[sid]
 
+@sio.event
+def kill(message):
+    kill_tab.text = 'KILL'
+
+    invoke(clear_kill_tab, delay=1)
+
 
 @sio.event
 def move(data):
@@ -100,19 +108,20 @@ def connect():
 
 app = Ursina()
 
-fog = 0.5
-speed = 6
-forest = True
+fog = 0
+speed = 50
+forest = False
 max_recoil = 120
 recoil_per_shot = 6
 recoil_recovery_speed = 14
 current_recoil = 0
 recoil_angle = 5
 recoil_time = 0.05
-fire_rate = 0.14
+fire_rate = 0
 last_shot_time = 0
 magazine_size = 0
 magazine = magazine_size
+weapon_distance = 0
 
 scope = False
 
@@ -140,6 +149,11 @@ running_sound = Audio('assets/sounds/running.mp3', autoplay=False, loop=True, vo
 
 ambient_rain = Audio('assets/sounds/ambient_rain.mp3', autoplay=True, loop=True, volume=.3)
 
+kill_tab = Text(parrent=camera, scale=1, color=color.red, origin=(-0.6, -16.3), text='')
+
+def clear_kill_tab():
+   kill_tab.text = ""
+
 forest_model = Entity(model='assets/models/forest.glb',y=2,shader=lit_with_shadows_fog_shader,scale=7.6, visible=forest, enabled=True)
 forest_model.set_shader_input("camera_pos", camera.world_position)
 forest_model.set_shader_input("fog_color", Vec4(0,0,0,1))
@@ -166,6 +180,7 @@ def settings():
     scene.fog_density = fog + 0.04
 
     window.fullscreen = False
+    window.cog_menu = False
     window.entity_counter.enabled = False
     window.collider_counter.enabled = False
 
@@ -204,20 +219,68 @@ ammo_text = Text(
 )
 
 ammo_9m = Entity(model='assets/models/9mm_ammo_box.glb', scale=.0008, position=Vec3(62, 2.9, -7), collider='box', enabled=False)
-pistol = Entity(parent=scene, model='assets/models/pm.glb', origin_y=-.5, collider='box', position=Vec3(61, 2.6, -5.6), scale=.007, rotation_z=78, rotation_x=90, rotation_y=104, enabled=False)
+pm = Entity(parent=scene, model='assets/models/pm.glb', origin_y=-.5, collider='box', position=Vec3(61, 2.6, -5.6), scale=.007, rotation_z=78, rotation_x=90, rotation_y=104, enabled=False)
+
+goldEagle = Entity(parent=scene, model='assets/models/gold_deagle.glb', origin_y=-.5, collider='box', position=Vec3(70, 2.2, -52), scale=.0023, rotation=Vec3(0, 0, 0), enabled=True)
+
+def get_deagle():
+    global magazine, magazine_size, speed, JSON_settings, fire_rate, recoil_time, weapon_distance
+
+    pick_up_sound.play()
+
+    fire_rate = 0.4
+    weapon_distance = 600
+
+    if player.weapon:
+        player.weapon.enabled = False
+
+    recoil_time = 0.2
+    goldEagle.parent = camera
+    goldEagle.collider = None
+    goldEagle.position = Vec3(.6, -.16, .33)
+    goldEagle.rotation = Vec3(0, 178, -.1)
+    player.weapon = goldEagle
+    player.weapon_name = 'gold_deagle'
+    magazine_size = 6
+    magazine = magazine_size
+    speed -= 0.6
+
+    JSON_settings["game_settings"]["weapon"] = 'gold_deagle'
+    JSON_settings["game_settings"]["magazine"] = magazine
+    JSON_settings["game_settings"]["max_bullets"] = max_bullets
+
+    shoot_sound.clip = 'assets/sounds/deagle-shot-sound.mp3'
+
+    with open('settings.json', 'w', encoding='utf-8') as f:
+        json.dump(JSON_settings, f, indent=4, ensure_ascii=False)
+goldEagle.on_click = get_deagle
 
 if tutorial and JSON_settings["game_settings"]["weapon"] == None:
     ammo_9m.enabled = True
-    pistol.enabled = True
+    pm.enabled = True
 elif tutorial and JSON_settings["game_settings"]["weapon"] == 'pm':
-    pistol.enabled = True
-    pistol.parent = camera
-    pistol.collider = None
-    pistol.position = Vec3(.5,-.24,.5)
-    pistol.rotation = Vec3(0, 86, -1)
+    weapon_distance = 100
+    pm.enabled = True
+    pm.parent = camera
+    pm.collider = None
+    pm.position = Vec3(.5,-.26,.5)
+    pm.rotation = Vec3(0, 86, -1)
     magazine_size = 12
     magazine = JSON_settings["game_settings"]["magazine"]
     speed -= 0.4
+    fire_rate = 0.17
+elif tutorial and JSON_settings["game_settings"]["weapon"] == 'gold_deagle':
+    weapon_distance = 600
+    recoil_time = 0.2
+    goldEagle.parent = camera
+    goldEagle.collider = None
+    goldEagle.position = Vec3(.6, -.16, .33)
+    goldEagle.rotation = Vec3(0, 178, -.1)
+    magazine_size = 6
+    magazine = JSON_settings["game_settings"]["magazine"]
+    speed -= 0.6
+    fire_rate = 0.4
+    shoot_sound.clip = 'assets/sounds/deagle-shot-sound.mp3'
 
 first_door_key = Entity(model='assets/models/door_key.glb', scale=.01, collider='box', position=Vec3(-28.65, 2, 13.), parent=scene, enabled=True)
 first_door_key.shader = lit_with_shadows_shader
@@ -247,26 +310,32 @@ def open_first_door():
         tutorial = True
 
         ammo_9m.enabled = True
-        pistol.enabled = True
+        pm.enabled = True
 
         with open('settings.json', 'w', encoding='utf-8') as f:
             json.dump(JSON_settings, f, indent=4, ensure_ascii=False)
 
 big_metal_door.on_click = open_first_door
 
-def get_pistol():
-    global magazine, magazine_size, speed, JSON_settings
+def get_pm():
+    global magazine, magazine_size, speed, JSON_settings, fire_rate, weapon_distance
 
     pick_up_sound.play()
 
-    pistol.parent = camera
-    pistol.collider = None
-    pistol.position = Vec3(.5,-.24,.5)
-    pistol.rotation = Vec3(0, 86, -1)
-    player.weapon = pistol
+    if player.weapon:
+        player.weapon.enabled = False
+
+    weapon_distance = 100
+    pm.parent = camera
+    pm.collider = None
+    pm.position = Vec3(.5,-.26,.5)
+    pm.rotation = Vec3(0, 86, -1)
+    player.weapon = pm
+    player.weapon_name = 'pm'
     magazine_size = 12
     magazine = magazine_size
     speed -= 0.4
+    fire_rate = 0.17
 
     JSON_settings["game_settings"]["weapon"] = 'pm'
     JSON_settings["game_settings"]["magazine"] = magazine
@@ -275,7 +344,7 @@ def get_pistol():
     with open('settings.json', 'w', encoding='utf-8') as f:
         json.dump(JSON_settings, f, indent=4, ensure_ascii=False)
 
-pistol.on_click = get_pistol
+pm.on_click = get_pm
 
 reload_time = 3
 reloading = False
@@ -323,7 +392,7 @@ def finish_reload():
 run = False
 
 def input(key):
-    global last_shot_time, current_recoil, magazine, reloading, run, JSON_settings
+    global last_shot_time, current_recoil, magazine, reloading, run, JSON_settings, weapon_distance
 
     now = time.time()
 
@@ -334,10 +403,9 @@ def input(key):
         json.dump(JSON_settings, f, indent=4, ensure_ascii=False)
 
     if key == 'left mouse down' and player.weapon and magazine > 0 and not reloading:
-        fire_rate = 0.17
         if now - last_shot_time >= fire_rate:
-            bullet = Entity(model='sphere', scale=.09, color=color.black, position=pistol.world_position + Vec3(.3, .5, 0))
-            bullet.animate_position(bullet.position + (camera.forward * 100), curve=curve.linear, duration=.7)
+            bullet = Entity(model='sphere', scale=.09, color=color.black, position=player.weapon.world_position + Vec3(.3, .5, 0))
+            bullet.animate_position(bullet.position + (camera.forward * 500), curve=curve.linear, duration=.7)
             destroy(bullet, delay=2)
             last_shot_time = now
             magazine -= 1
@@ -347,20 +415,23 @@ def input(key):
             if current_recoil > max_recoil:
                 current_recoil = max_recoil
 
-        ray = raycast(player.position, camera.forward, distance=100, ignore=[camera, player, forest, ground, field_gate_border_collider])
+            ray = raycast(player.position, camera.forward, distance=weapon_distance, ignore=[camera, player, forest, ground, field_gate_border_collider])
 
-        weapon_name = player.weapon_name
+            weapon_name = player.weapon_name
 
-        for sid, ent in other_players.items():
-            if ray.entity is ent:
-                hit_sound.stop()
-                hit_sound.play()
-                sio.emit('hit', {'player': sid,'weapon': weapon_name,'distance': ray.distance})
+            for sid, ent in other_players.items():
+                if ray.entity is ent:
+                    hit_sound.stop()
+                    hit_sound.play()
+                    sio.emit('hit', {'player': sid, 'weapon': weapon_name, 'distance': ray.distance})
 
-                print(f'HIT {sid} with {weapon_name}')
-
-        pistol.animate_rotation(pistol.rotation + Vec3(0,0,recoil_angle), duration=recoil_time, curve=curve.linear)
-        invoke(lambda: pistol.animate_rotation(pistol.rotation - Vec3(0,0,recoil_angle/1.47), duration=recoil_time, curve=curve.linear), delay=recoil_time)
+                    print(f'HIT {sid} with {weapon_name}')
+        if player.weapon_name == 'pm':
+            player.weapon.animate_rotation(player.weapon.rotation + Vec3(0,0,recoil_angle), duration=recoil_time, curve=curve.linear)
+            invoke(lambda: player.weapon.animate_rotation(player.weapon.rotation - Vec3(0,0,recoil_angle/1.47), duration=recoil_time, curve=curve.linear), delay=recoil_time/1.7)
+        else:
+            player.weapon.animate_rotation(player.weapon.rotation + Vec3(recoil_angle,0,0), duration=recoil_time, curve=curve.linear)
+            invoke(lambda: player.weapon.animate_rotation(player.weapon.rotation - Vec3(recoil_angle/1.47,0,0), duration=recoil_time, curve=curve.linear), delay=recoil_time/1.7)
 
     if magazine == 0 and max_bullets == 0 and key == 'left mouse down' and not reloading and not run and player.weapon:
         empty_sound.stop()
@@ -402,7 +473,7 @@ def load_village():
         Entity(model='assets/models/concrete_wall_protection.glb', position=Vec3(73.2, 2, 14.19676), scale=1.5, collider='box', rotation_y=-3, rotation_x=3.4),
         Entity(model='assets/models/stalker_blockpost.glb', position=Vec3(130, 1.9, -15), rotation_y=-90, scale=43),
         Entity(model='assets/models/sandbag_wall.glb', position=Vec3(48.9, 2, 9), scale=1.9, rotation_y=-9, collider='box'),
-        Entity(model='assets/models/btr.glb', position=Vec3(59, 2, -44), scale=1.76, rotation_y=-101, collider='box'),
+        # Entity(model='assets/models/btr.glb', position=Vec3(59, 2, -44), scale=1.76, rotation_y=-101, collider='box'),
         Entity(model='assets/models/pallet_of_boxes.glb', position=Vec3(49, 2, -41), scale=3, rotation_y=1.2, collider='box'),
         Entity(model='assets/models/field.glb', scale=20, position=Vec3(54, 2, -51)),
         Entity(model='assets/models/field.glb', scale=20, position=Vec3(74, 2, -51)),
@@ -457,17 +528,40 @@ def load_first_scene():
 
 village_spawn = False
 run_sound_flag = False
-sneak = False
+sneak_flag = False
+
+def sneak():
+   player.camera_pivot.y = 2.2 - held_keys['left control']
+   player.height = 2 - held_keys['left control']
+   player.speed = speed - 2
+
+   ray = raycast(origin=camera.world_position, direction=Vec3(0, 1, 0), distance=1, ignore=[camera, player])
+   if (ray.hit):
+      player.jump_height = 0
+   else:
+      player.jump_height = 1.55
+
+def stay():
+   global sneak_flag
+
+   ray = raycast(origin=camera.world_position, direction=Vec3(0, 1, 0), distance=1, ignore=[camera, player])
+
+   if (not ray.hit):
+      player.camera_pivot.y = 2.7 - held_keys['left control']
+      player.height = 2 - held_keys['left control']
+      player.speed = speed + 2
+      sneak_flag = True
 
 def update():
-    global current_recoil, fog, village_spawn, run_sound_flag, scope, reloading, run, tutorial, health, speed, sneak, move_window
+    global current_recoil, fog, village_spawn, run_sound_flag, scope, reloading, run, tutorial, health, speed, sneak_flag, move_window
 
     if health <= 0:
         player.position = Vec3(52, 2.4, -18)
+        sio.emit('kill', {'msg': 'killed'})
         health = 100
 
     if tutorial:
-        if not sneak:
+        if not sneak_flag:
             sio.emit('move', {'x': player.x, 'y': player.y, 'z': player.z, 'ry': player.rotation.y})
         else:
             sio.emit('move', {'x': player.x, 'y': player.y - 1.4, 'z': player.z, 'ry': player.rotation.y})
@@ -495,14 +589,23 @@ def update():
     current_recoil = lerp(current_recoil, 0, time.dt * (recoil_recovery_speed/2))
 
     if held_keys['right mouse'] and player.weapon and not reloading:
-        scope = True
-        pistol.position = Vec3(-0.0217, -.432 - current_recoil*0.01, .5)
-        pistol.rotation = Vec3(0, 90, .00001)
-    else:
-        if player.weapon:
+        if player.weapon_name == 'pm':
+            scope = True
+            pm.position = Vec3(-0.0217, -.432 - current_recoil*0.01, .5)
+            pm.rotation = Vec3(0, 90, .00001)
+        elif player.weapon_name == 'gold_deagle':
+            scope = True
+            goldEagle.position = Vec3(0, -.205 - current_recoil*0.01, .35)
+            goldEagle.rotation = Vec3(0, 180, 0)
+    elif player.weapon:
+        if player.weapon_name == 'pm':
             scope = False
-            pistol.position = Vec3(.5, -.24, .5)
-            pistol.rotation = Vec3(0, 86, -1)
+            goldEagle.position = Vec3(.5, -.24, .5)
+            goldEagle.rotation = Vec3(0, 86, -1)
+        elif player.weapon_name == 'gold_deagle':
+            scope = False
+            goldEagle.position = Vec3(.6, -.16, .33)
+            goldEagle.rotation = Vec3(0, 178, -.1)
 
     if player.x > 49 and not village_spawn:
         village_spawn = True
@@ -542,7 +645,7 @@ def update():
         camera.fov = 145
         running_sound.pitch = 1.6
         run = True
-    elif scope and not reloading and player.weapon_name == 'pm':
+    elif scope and not reloading and player.weapon:
         player.speed = speed - 1.5
         camera.fov = 115
         running_sound.pitch = .7
@@ -552,17 +655,13 @@ def update():
         camera.fov = 130
         running_sound.pitch = 1
 
-    if held_keys['control'] and not sneak:
-        player.camera_pivot.y = 2.2
-        player.height = 1
-        speed -= 2
+    if held_keys['control'] and not sneak_flag:
+        sneak()
         running_sound.pitch = 1.45
-        sneak = True
-    elif sneak and not held_keys['control']:
-        player.camera_pivot.y = 2.7
-        player.height = 2
-        speed += 2
-        sneak = False
+        sneak_flag = True
+    elif sneak_flag and not held_keys['control']:
+        stay()
+        sneak_flag = False
 
     if player.y < -5:
         player.position = Vec3(player.x, 10, player.z)
@@ -580,8 +679,11 @@ if __name__ == '__main__':
 
     if not tutorial: player.weapon = None
     elif tutorial and JSON_settings["game_settings"]["weapon"] == 'pm':
-        player.weapon = pistol
+        player.weapon = pm
         player.weapon_name = "pm"
+    elif tutorial and JSON_settings["game_settings"]["weapon"] == 'gold_deagle':
+        player.weapon = goldEagle
+        player.weapon_name = 'gold_deagle'
 
     player.first_door_key = None
 
